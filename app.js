c73522733618d3983559243cd2ea6d4a2a1f03b0
karlmiche <karlymism@gmail.com>
2020-06-10 13:39:46 -0700

    cuter styling now, date object in js

--- app.js
+++ app.js
@@ -7,25 +7,50 @@ const burrito = document.getElementById("burrito");
 const pill = document.getElementById("pill");
 
 //canvas details
-canvas.width = 700;
+canvas.width = 400;
 canvas.height = 400;
 
 
 //Billy the Pet
 window.onload = function() {
-    ctx.drawImage(pet, 450, 150);
+    ctx.drawImage(pet, 50, 90);
   };
+
+ctx.font = "14px monospace";
+ctx.fillText("Pet is watching you intently...", 50, 50)
 
 //********Local Storage Information**************/
 
 /************dateObject functions**********/
-Date.now()
+//this will update the time on the page and internally
+//so we can base conditional logic off of what time it IS
+
+function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+function gettingMinutes() {
+    var d = new Date();
+    var t = document.getElementById("time");
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var s = addZero(d.getSeconds());
+    t.innerHTML = h + ":" + m + ":" + s;
+
+    setTimeout(gettingMinutes, 1000);
+  }
+
+  gettingMinutes();
+  console.log(gettingMinutes);
 
 /*****Initalize Counters for needs**************/
-let happiness = 50;
-let health = 50;
-let hunger = 50;
-let clean = 50;
+let happiness = 100;
+let health = 100;
+let hunger = 100;
+let clean = 100;
 
 /*********** Decrementing intervals for needs*******/
 function lessHappy () {}
